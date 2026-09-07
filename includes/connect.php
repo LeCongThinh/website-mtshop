f86bf2417ac5d8d0df21bbbbea9aa134301a0171
Nguyen The Vy <130385119+NguyenTheVy2003@users.noreply.github.com>
2026-09-07 07:15:55 +0700

    update name database connect

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = new mysqli('localhost', 'root', '', 'website_mtshop');
+$con = new mysqli('localhost', 'root', '', 'website_mtshop.sql');
 $con->set_charset("utf8");
 if ($con->connect_error) {
     die("Kết nối thất bại   : " . $con->connect_error);
